32e97d0411257d122f5809334ebce35239f6a759
Pete <peter.t.connor@gmail.com>
2020-03-22 22:24:32 -0400

    Initial Commit

---
+++ ReleaseDate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReleaseDate
+//
+//  Created by Pete Connor on 3/22/20.
+//  Copyright © 2020 Pete Connor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
